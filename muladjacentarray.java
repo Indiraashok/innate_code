0db06c184f7b71c56e311112f714b9c5bee23242
Indiraashok <159457087+Indiraashok@users.noreply.github.com>
2025-08-07 11:15:44 +0530

    Create muladjacentarray.java

---
+++ muladjacentarray.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        
+        for(int i=0;i<n;i++)
+            a[i]=s.nextInt();
+        System.out.print(a[0]*a[1]+" ");
+        for(int i=1;i<n-1;i++)
+        {
+            System.out.print(a[i-1]*a[i+1]+" ");
+        }
+        System.out.print(a[n-2]*a[n-1]);
+    }
+}
